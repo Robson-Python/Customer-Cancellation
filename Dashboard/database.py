ef9b032fce392cc985464c215ec4cfb346481991
Robson <robsonprogramadordelphi@gmail.com>
2025-02-28 12:37:47 -0400

    Add files via upload

--- Dashboard/database.py
+++ Dashboard/database.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
 
-df_cancelamentos = pd.read_csv("cancelamentos_analisado.csv")
-df_previsao = pd.read_csv("previsao_cancelamentos.csv")
-df_modelo = pd.read_csv("modelo_previsao.csv")
+df_cancelamentos = pd.read_csv("/workspaces/Customer-Cancellation/Dashboard/cancelamentos_analisado.csv")
+df_previsao = pd.read_csv("/workspaces/Customer-Cancellation/Dashboard/previsao_cancelamentos.csv")
+df_modelo = pd.read_csv("/workspaces/Customer-Cancellation/Dashboard/modelo_previsao.csv")
