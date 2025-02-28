dc1485292216d8b90fd07cf1a67afbc4a0d69594
Robson <robsonprogramadordelphi@gmail.com>
2025-02-28 14:51:01 -0400

    Add files via upload

--- Dashboard/database.py
+++ Dashboard/database.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
 
-df_cancelamentos = pd.read_csv("/workspaces/Customer-Cancellation/Dashboard/cancelamentos_analisado.csv")
-df_previsao = pd.read_csv("/workspaces/Customer-Cancellation/Dashboard/previsao_cancelamentos.csv")
-df_modelo = pd.read_csv("/workspaces/Customer-Cancellation/Dashboard/modelo_previsao.csv")
+df_cancelamentos = pd.read_csv("https://github.com/Robson-Python/Customer-Cancellation/blob/main/Dashboard/cancelamentos_analisado.csv")
+df_previsao = pd.read_csv("https://github.com/Robson-Python/Customer-Cancellation/blob/main/Dashboard/previsao_cancelamentos.csv")
+df_modelo = pd.read_csv("https://github.com/Robson-Python/Customer-Cancellation/blob/main/Dashboard/modelo_previsao.csv")
