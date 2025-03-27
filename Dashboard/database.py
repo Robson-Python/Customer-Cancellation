b96a2e3e8d9416d1449832d51846f06a2bfc0c54
Robson <robsonprogramadordelphi@gmail.com>
2025-03-27 11:10:26 -0400

    Update database.py

--- Dashboard/database.py
+++ Dashboard/database.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
-df_cancelamentos = pd.read_csv("https://raw.githubusercontent.com/Robson-Python/Customer-Cancellation/main/Dashboard/cancelamentos_analisado.csv")
-df_previsao = pd.read_csv("https://raw.githubusercontent.com/Robson-Python/Customer-Cancellation/main/Dashboard/previsao_cancelamentos.csv")
-df_modelo = pd.read_csv("https://raw.githubusercontent.com/Robson-Python/Customer-Cancellation/main/Dashboard/modelo_previsao.csv")
+df_cancelamentos = pd.read_csv("https://raw.githubusercontent.com/Robson-Python/Customer-Cancellation/refs/heads/main/Dashboard/cancelamentos_analisado.csv")
+df_previsao = pd.read_csv("https://raw.githubusercontent.com/Robson-Python/Customer-Cancellation/refs/heads/main/Dashboard/previsao_cancelamentos.csv")
+df_modelo = pd.read_csv("https://raw.githubusercontent.com/Robson-Python/Customer-Cancellation/refs/heads/main/Dashboard/modelo_previsao.csv")
+
